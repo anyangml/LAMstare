66bf29d5aef050e0ec500c1e4453fa57320c1595
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2024-10-28 12:58:32 +0800

    chore: refactor

--- lamstare/release/run_ood_test.py
+++ lamstare/release/run_ood_test.py
@@ -1,8 +1,9 @@
 import yaml
 from typing import Optional, Tuple, Dict
-from lamstare.utils.dptest import extract_ood_test_pth_from_yml
+from lamstare.utils.dptest import extract_ood_test_pth_from_yml, get_head_weights
 from lamstare.infra.ood_database import OODRecord
 import numpy as np
+
 def get_ood_to_head_map(mapping_path: str = "OOD_DATASET.yml", output_path:str = "./testood", overwrite:Optional[bool]=False) -> Tuple[Dict[str, str], bool]:
     """
     Load the OOD test input yaml file, prepare dptest files, return a `OOD dataset -> task head` mapping.
@@ -47,22 +48,21 @@ def run_ood_test_single_task(exp_path: str, ckpt: Optional[int] = None, head: Op
     """
     pass
 
-def run_ood_test_multitask(exp_path: str, mapping:dict, model_version:str, ckpt: Optional[int] = None)->None:
+def run_ood_test(exp_path: str, mapping:dict, model_version:str, ckpt: Optional[int] = None)->None:
     """
     This should be a loop through all heads and call run_ood_test_single_task.
     """
     run_id=exp_path.split("/")[-1] # Get basename as id
     for ood_dataset, head in mapping.items():
         temp_file_name = f"{run_id}#{ckpt}#{ood_dataset}#{head}"
-        temp_pattern = f"{run_id}#{ckpt}#{ood_dataset}%"
-        if len(OODRecord.query_like_run_name(pattern=temp_pattern)) == 0:
+        if len(OODRecord.query_by_name(run_name=temp_file_name)) == 0:
             head_dptest_res = run_ood_test_single_task(exp_path, ckpt, head)
 
-            if np.isnan(head_dptest_res[f"{head}  Virial MAE"]):
-                        head_dptest_res[f"{head}  Virial MAE"] = -1
-                        head_dptest_res[f"{head}  Virial RMSE"] = -1
-                        head_dptest_res[f"{head}  Virial MAE/Natoms"] = -1
-                        head_dptest_res[f"{head}  Virial RMSE/Natoms"] = -1
+            if np.isnan(head_dptest_res[f"{ood_dataset}  Virial MAE"]):
+                        head_dptest_res[f"{ood_dataset}  Virial MAE"] = -1
+                        head_dptest_res[f"{ood_dataset}  Virial RMSE"] = -1
+                        head_dptest_res[f"{ood_dataset}  Virial MAE/Natoms"] = -1
+                        head_dptest_res[f"{ood_dataset}  Virial RMSE/Natoms"] = -1
 
             OODRecord(
                 run_id=run_id, 
@@ -71,18 +71,21 @@ def run_ood_test_multitask(exp_path: str, mapping:dict, model_version:str, ckpt:
                 ood_dataset=ood_dataset,
                 step=ckpt, 
                 head=head,
-                energy_mae=head_dptest_res[f"{head}  Energy MAE"],
-                energy_rmse=head_dptest_res[f"{head}  Energy RMSE"],
-                energy_mae_natoms=head_dptest_res[f"{head}  Energy MAE/Natoms"],
-                energy_rmse_natoms=head_dptest_res[f"{head}  Energy RMSE/Natoms"],
-                force_mae=head_dptest_res[f"{head}  Force  MAE"],
-                force_rmse=head_dptest_res[f"{head}  Force  RMSE"],
-                virial_mae=head_dptest_res[f"{head}  Virial MAE"],
-                virial_rmse=head_dptest_res[f"{head}  Virial RMSE"],
-                virial_mae_natoms=head_dptest_res[f"{head}  Virial MAE/Natoms"],
-                virial_rmse_natoms=head_dptest_res[f"{head}  Virial RMSE/Natoms"],
+                energy_mae=head_dptest_res[f"{ood_dataset}  Energy MAE"],
+                energy_rmse=head_dptest_res[f"{ood_dataset}  Energy RMSE"],
+                energy_mae_natoms=head_dptest_res[f"{ood_dataset}  Energy MAE/Natoms"],
+                energy_rmse_natoms=head_dptest_res[f"{ood_dataset}  Energy RMSE/Natoms"],
+                force_mae=head_dptest_res[f"{ood_dataset}  Force  MAE"],
+                force_rmse=head_dptest_res[f"{ood_dataset}  Force  RMSE"],
+                virial_mae=head_dptest_res[f"{ood_dataset}  Virial MAE"],
+                virial_rmse=head_dptest_res[f"{ood_dataset}  Virial RMSE"],
+                virial_mae_natoms=head_dptest_res[f"{ood_dataset}  Virial MAE/Natoms"],
+                virial_rmse_natoms=head_dptest_res[f"{ood_dataset}  Virial RMSE/Natoms"],
             ).insert()
-    pass
+        elif len(OODRecord.query_by_name(run_name=temp_file_name)) == 1:
+            continue
+        else:
+            print(f"{temp_file_name} has multiple records, please check!!!")
 
 
 def get_latest_ckpt(exp_path: str)->int:
@@ -104,7 +107,7 @@ def main(
     )->None:
 
     # generate test file from input.json
-    mapping, is_multitask = get_ood_to_head_map()
+    mapping, is_multitask = get_ood_to_head_map(mapping_path=mapping_path, output_path=output_path, overwrite=overwrite)
     
     if ckpt is None:
         ckpt = get_latest_ckpt(exp_path)
@@ -112,10 +115,12 @@ def main(
         ckpt = ckpt
 
     if is_multitask:
-        # assert all heads are available in ckpt 
-        pass #call test multitask run_ood_test_multitask
+        avaliable_heads_in_model = set(get_head_weights(exp_path).keys())
+        heads_needed = set(mapping.values())
+        assert heads_needed.issubset(avaliable_heads_in_model), f"Missing heads in model: {heads_needed - avaliable_heads_in_model}"
+        run_ood_test(exp_path, mapping, model_version, ckpt)
     else:
-        pass #call test single task run_ood_test_single_task
+       run_ood_test(exp_path, mapping, model_version, ckpt)
 
 if __name__ == "__main__":
     # main("", is_multitask=False)
