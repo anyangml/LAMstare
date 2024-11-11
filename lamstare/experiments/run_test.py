9267733b8e27cf098fbac495a3b40e922851a4da
caic99 <amoycaic@gmail.com>
2024-11-11 14:51:28 +0800

    rename functions

--- lamstare/experiments/run_test.py
+++ lamstare/experiments/run_test.py
@@ -1,26 +1,27 @@
 import logging
-from pathlib import Path
-from typing import Optional
-from lamstare.utils.dlc_submit import query_job_numbers, submit_job_to_dlc
-from lamstare.utils.dptest import (
-    get_head_weights,
-    extract_valid_path_from_input,
-)
-from lamstare.infra import Record
-from dotenv import load_dotenv # type: ignore
 import os
 import sys
+from pathlib import Path
+from typing import Optional
+
+from dotenv import load_dotenv  # type: ignore
 load_dotenv()
 
+from lamstare.infra import Record
+from lamstare.utils.dlc_submit import query_job_numbers, submit_job_to_dlc
+from lamstare.utils.dptest import extract_valid_path_from_input, get_head_weights
+
+# Deprecated
 temp_file_path = os.environ.get("TEMP_FILE_DIR")
 assert temp_file_path is not None, "TEMP_FILE_DIR is not set in .env"
 
-def dptest_one_cpkt_on_all_heads(exp_path:str, step: int):
-    run_id=exp_path.split("/")[-1] # Get basename as id
+
+def submit_ind_test(exp_path: str, step: int):
+    run_id = exp_path.split("/")[-1]  # Get basename as id
     try:
         heads = list(get_head_weights(exp_path).keys())
     except KeyError:
-        heads = [""] # single task
+        heads = [""]  # single task
 
     for head in heads:
         run_name = f"{run_id}#{step}#{head}"
@@ -47,33 +48,36 @@ def dptest_one_cpkt_on_all_heads(exp_path:str, step: int):
             logging.error(f"ERROR: {run_name} has multiple records, please check.")
 
 
-def find_ckpt_to_test_cron(exp_path:str, freq:int) -> Optional[int]:
+def find_ckpt_to_test_cron(exp_path: str, freq: int) -> Optional[int]:
     ckpt_pth = Path(exp_path)
-    with open(ckpt_pth / "checkpoint","r") as f:
+    with open(ckpt_pth / "checkpoint", "r") as f:
         latest_ckpt = int(f.readlines()[0].split("-")[1].split(".")[0])
     # Get exp_path abs path
     exp_path = str(ckpt_pth.resolve())
     # Get basename
     run_id = exp_path.split("/")[-1]
     if len(Record.query(run_id=run_id)) > 0:
-        previous_tested_step = int(Record.query(run_id=run_id)[-1].step) # type: ignore
+        previous_tested_step = int(Record.query(run_id=run_id)[-1].step)  # type: ignore
     else:
         previous_tested_step = 0
-    print(f"Latest ckpt tested: {previous_tested_step}, Latest ckpt available: {latest_ckpt}\n")
+    print(
+        f"Latest ckpt tested: {previous_tested_step}, Latest ckpt available: {latest_ckpt}\n"
+    )
     if latest_ckpt >= previous_tested_step + freq:
         need_to_test = previous_tested_step + freq
     else:
         need_to_test = None
-
     return need_to_test
 
-def main(exp_path:str, freq:int=200000):
+
+def main(exp_path: str, freq: int = 200000):
     need_to_test = find_ckpt_to_test_cron(exp_path, freq)
     if need_to_test is not None:
         print(f"Running DPTEST for {exp_path} on ckpt-{need_to_test}...\n")
-        dptest_one_cpkt_on_all_heads(exp_path, need_to_test)
+        submit_ind_test(exp_path, need_to_test)
     else:
         print("No new ckpt to test.\n")
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
